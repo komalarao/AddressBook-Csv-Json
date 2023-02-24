6898a466d6df93b264fb470ca0e5e06a4c177bcf
komalarao <manoharvakamullu1999@gmail.com>
2023-02-24 01:47:31 -0800

    added: program for perform operations on csv file

---
+++ src/main/java/com/bridgelabz/fileIO/AddressBook.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.fileIO;
+
+import java.io.*;
+
+public class AddressBookService {
+	 public static void main(String[] args) throws IOException {
+	        System.out.println("Welcome to the Address book System by using IO.");
+	        Operations operations = new Operations();
+	        System.out.println("-----------------------------*****-------------------------");
+	        operations.AddressBook(operations);
+	    }
+}
